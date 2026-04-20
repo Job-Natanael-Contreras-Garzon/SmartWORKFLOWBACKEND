a412ecb4fe4dd7ae2b5be5aaf5c27e84663f78b9
Job Natanael Contreras Garzon <143237173+Job-Natanael-Contreras-Garzon@users.noreply.github.com>
2026-04-20 10:48:15 -0400

    diseno de arquitectura

---
+++ backend - springboot/smartworkflow/src/main/java/com/example/smartworkflow/SmartworkflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartworkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartworkflowApplication.class, args);
+	}
+
+}
---
+++ backend - springboot/smartworkflow/src/test/java/com/example/smartworkflow/SmartworkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartworkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
